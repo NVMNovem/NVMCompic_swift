414738db387ad631adf4634937a9e56db5fe8814
Damian Van de Kauter <vdkdamian@gmail.com>
2022-03-13 20:43:52 +0100

    Test

--- Sources/NVMCompic/NVMCompic.swift
+++ Sources/NVMCompic/NVMCompic.swift
@@ -1,6 +1,90 @@
+import Foundation
+@available(iOS 15.0, macOS 12.0, *)
 public struct NVMCompic {
     public private(set) var text = "Hello, World!"
+    
+    static private let session = URLSession.shared
 
     public init() {
     }
+    
+    public static func getCompicResults() async throws {
+        print("test COMPIC")
+        let headers = ["content-type": "application/json"]
+        let body = ["url" : "2dehands.be"]
+        
+        var request = URLRequest(url: URL(string: "https://glacial-reaches-72317.herokuapp.com/api")!)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = headers
+        request.httpBody = body.percentEncoded()
+        
+            print("test COMPIC 1")
+        do {
+            let (data, response) = try await session.data(for: request)
+            print("test COMPIC 2")
+            print(data)
+            print(response)
+        } catch {
+            print("error COMPIC: \(error)")
+        }
+        
+    }
+    
+    public enum ImageType: String {
+        case png = "png"
+        case jpeg = "jpeg"
+        case tiff = "tiff"
+    }
+    
+    public enum ResizeMode: String {
+        /**
+         Preserving aspect ratio, ensure the image covers both provided dimensions by cropping/clipping to fit.
+         
+         - note: This is the default
+         */
+        case cover = "cover"
+        
+        /**
+         Preserving aspect ratio, contain within both provided dimensions using "letterboxing" where necessary.
+         */
+        case contain = "contain"
+        
+        /**
+         Ignore the aspect ratio of the input and stretch to both provided dimensions.
+         */
+        case fill = "fill"
+        
+        /**
+         Preserving aspect ratio, resize the image to be as large as possible while ensuring its dimensions are less than or equal to both those specified.
+         */
+        case inside = "inside"
+        
+        /**
+         Preserving aspect ratio, resize the image to be as small as possible while ensuring its dimensions are greater than or equal to both those specified.
+         */
+        case outside = "outside"
+    }
+}
+
+extension Dictionary {
+    func percentEncoded() -> Data? {
+        return map { key, value in
+            let escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
+            return escapedKey + "=" + escapedValue
+        }
+        .joined(separator: "&")
+        .data(using: .utf8)
+    }
+}
+
+extension CharacterSet {
+    static let urlQueryValueAllowed: CharacterSet = {
+        let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
+        let subDelimitersToEncode = "!$&'()*+,;="
+
+        var allowed = CharacterSet.urlQueryAllowed
+        allowed.remove(charactersIn: generalDelimitersToEncode + subDelimitersToEncode)
+        return allowed
+    }()
 }
--- Tests/NVMCompicTests/NVMCompicTests.swift
+++ Tests/NVMCompicTests/NVMCompicTests.swift
@@ -1,11 +1,6 @@
+import Foundation
 import XCTest
 @testable import NVMCompic
 
 final class NVMCompicTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(NVMCompic().text, "Hello, World!")
-    }
 }
