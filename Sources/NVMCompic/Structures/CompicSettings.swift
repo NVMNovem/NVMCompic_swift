ed5b83e7cf9bbacaa97146b378b4198602059a58
Damian Van de Kauter <vdkdamian@gmail.com>
2022-03-30 22:21:06 +0200

    Fix

--- Sources/NVMCompic/Structures/CompicSettings.swift
+++ Sources/NVMCompic/Structures/CompicSettings.swift
@@ -17,28 +17,36 @@ public struct CompicSettings {
     /**
      Set this option if you want to remove unused compics after a specific amount of time.
      */
-    public var clearUnusedCompics: After = .never
+    public var clearUnusedCompics: After
+    
+    
+    public init(compicPath: URL,
+                clearUnusedCompics: After = .never) {
+        self.compicPath = compicPath
+        self.clearUnusedCompics = clearUnusedCompics
+    }
+    
     
     public enum After {
         
         /**
          Use this to indicate that stored compics may never be deleted on the device.
          */
-        case .never
+        case never
         
         /**
          Use this to indicate that stored compics may be deleted after a week on the device.
          */
-        case .week
+        case week
         
         /**
          Use this to indicate that stored compics may be deleted after a month on the device.
          */
-        case .month
+        case month
         
         /**
          Use this to indicate that stored compics may be deleted after a year on the device.
          */
-        case .year
+        case year
     }
 }
