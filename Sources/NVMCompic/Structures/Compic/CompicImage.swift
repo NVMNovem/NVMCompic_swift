2b9a4d55b2c777ab094d2cf09ecfe7b78c54915f
Damian Van de Kauter <vdkdamian@gmail.com>
2022-04-04 22:38:27 +0200

    Added CompicImage

---
+++ Sources/NVMCompic/Structures/Compic/CompicImage.swift
@@ -0,0 +1,25 @@
+//
+//  CompicImage.swift
+//  
+//
+//  Created by Damian Van de Kauter on 04/04/2022.
+//
+
+import Foundation
+
+public struct CompicImage: Codable {
+    public var type: ImageType
+    public var data: Data
+    public var format: NVMCompic.ImageType?
+    public var resizeType: NVMCompic.ResizeType?
+    public var width: Int?
+    public var height: Int?
+    
+    public enum ImageType: String, Codable {
+        case icon
+        case background
+        case card
+    }
+}
+
+
